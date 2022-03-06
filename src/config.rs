3e9b59df5cec084dbb2ed8726f3ae7385d25909b
vduseev <vagiz@duseev.com>
2022-03-06 14:31:27 +0100

    Listing input and output files using glob
    Closes #10

--- src/config.rs
+++ src/config.rs
@@ -11,37 +11,23 @@ pub struct Config {
 }
 
 pub fn parse_config(filename: &str) -> Config {
-    // Default
+    // Default config
     let default_config = Config {
         command: "a".to_string(),
         input: "b".to_string(),
         output: "c".to_string(),
     };
 
-    // Open file
     let path = Path::new(filename);
     let disp = path.display();
-    // let file = match File::open(&path) {
-    //     Err(why) => panic!("couldn't open config file {}: {}", disp, why),
-    //     Ok(file) => file,
-    // };
-
-    // let mut contents = String::new();
-    // match file.read_to_string(&mut contents) {
-    //     Err(why) => panic!("couldn't read config file {}: {}", disp, why),
-    //     Ok(_) => println!("Parsed config file {}", disp),
-    // };
 
     let contents = match fs::read_to_string(path) {
-        // Err(why) => panic!(),
         Err(why) => {
             eprintln!("couldn't read config file {}: {}", disp, why);
             return default_config;
         }
         Ok(s) => s,
     };
-
-    // let s = contents.as_str();
 
     match serde_yaml::from_str(&contents) {
         Err(why) => panic!("couldn't parse config file: {}", why),
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod config;
+mod tests;
 
 fn main() {
     let config = config::parse_config("tad.yml");
+
+    // Collect input files
+    tests::collect(&config.input, &config.output);
 
     println!("config:\n{}", config);
 }
